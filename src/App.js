a3590547e31e85a2e53a67ee50155ab009fd29bd
garrison90 <62501286+garrison90@users.noreply.github.com>
2020-05-27 00:36:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Modal from "./Modal";
+import ContactsList from "./ContactsList";
+import "./styles/App.css";
+
+export default class App extends Component {
+  state = {
+    items: [
+      {
+        name: "Igor",
+        surname: "Ivanov",
+        phone: "0938472017",
+        id: 1,
+      },
+      {
+        name: "Andrey",
+        surname: "Petrov",
+        phone: "0963445511",
+        id: 2,
+      },
+      {
+        name: "Ivan",
+        surname: "Sidorov",
+        phone: "0968470215",
+        id: 3,
+      },
+    ],
+    show: false,
+  };
+
+  showModal = (e) => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  addNewContact = (newContact) => {
+    this.setState({
+      items: [...this.state.items, newContact],
+      show: false,
+    });
+  };
+
+  deleteContact = (id) => {
+    this.setState({
+      items: this.state.items.filter((item) => item.id !== id),
+    });
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <ContactsList
+          items={this.state.items}
+          deleteContact={this.deleteContact}
+        />
+        <button className="add-btn" onClick={() => this.showModal()}>
+          Add New Contact
+        </button>
+        <Modal
+          show={this.state.show}
+          onClose={this.showModal}
+          addNewContact={this.addNewContact}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/Modal.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import "./styles/Modal.css";
+
+const initialState = {
+  name: "",
+  surname: "",
+  phone: "",
+  nameError: "",
+  surnameError: "",
+  phoneError: "",
+  disabledButton: false,
+};
+
+export default class Modal extends Component {
+  state = initialState;
+
+  onClose = (e) => {
+    this.props.onClose(e);
+    this.setState(initialState);
+  };
+
+  handleChange = (e) => {
+    if ([e.target.value].length > 0) {
+      this.setState({
+        nameError: "",
+        surnameError: "",
+        phoneError: "",
+        disabledButton: false,
+      });
+    }
+
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  validate = () => {
+    let nameError = "";
+    let surnameError = "";
+    let phoneError = "";
+
+    if (!this.state.name) {
+      nameError = "This field is required";
+    }
+
+    if (!this.state.surname) {
+      surnameError = "This field is required";
+    }
+
+    if (!this.state.phone) {
+      phoneError = "This field is required";
+    }
+
+    if (nameError || surnameError || phoneError) {
+      this.setState({
+        nameError,
+        surnameError,
+        phoneError,
+        disabledButton: true,
+      });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, surname, phone } = this.state;
+    const isValid = this.validate();
+    if (isValid) {
+      const newContact = { name, surname, phone, id: Date.now };
+      this.props.addNewContact(newContact);
+      this.setState(initialState);
+    }
+  };
+
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+
+    return (
+      <div className="container">
+        <h1>Add Contact</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder="Enter name..."
+            className={this.state.nameError ? "txtb error" : "txtb"}
+          />
+
+          <input
+            name="surname"
+            value={this.state.surname}
+            onChange={this.handleChange}
+            placeholder="Enter surname..."
+            className={this.state.surnameError ? "txtb error" : "txtb"}
+          />
+
+          <input
+            name="phone"
+            value={this.state.phone}
+            onChange={this.handleChange}
+            placeholder="Enter pnone..."
+            className={this.state.phoneError ? "txtb error" : "txtb"}
+          />
+
+          <button className="add-form-btn" disabled={this.state.disabledButton}>
+            Add
+          </button>
+
+          <button className="cancel-btn" onClick={this.onClose}>
+            Cancel
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
